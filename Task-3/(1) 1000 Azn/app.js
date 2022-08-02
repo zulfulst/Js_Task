fe173de570f2ea7d162daff9719db01b0ce53c63
zulfulst <zulfulst@gmail.com>
2022-08-02 16:37:28 +0400

    complate

--- Task-3/(1) 1000 Azn/app.js
+++ Task-3/(1) 1000 Azn/app.js
@@ -1,8 +1,8 @@
-let eded = prompt(" Choose one number between 0 and 50 ")
-
+let num = prompt(" Choose one number between 0 and 50 ")
+let eded = Math.floor(num) // Tam edede cevirmek ucun
 if (eded > 50 || eded < 0) {
     console.log("Wrong choice")
-} else if (eded == 7) {
+} else if (eded == 49) {
     console.log('You win 1K Azn')
 } else {
     console.log('Check your luck again')
